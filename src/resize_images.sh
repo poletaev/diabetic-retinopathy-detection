edb2ef16a85e90e28336f656b46f99beba913b1f
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-07-26 18:55:52 +0600

    Don't enhance image at initial resize.

--- src/resize_images.sh
+++ src/resize_images.sh
@@ -9,7 +9,7 @@ if [ -z $1 ]; then
     exit -1
 fi
 
-SIZE=512x512
+SIZE=1024x1024
 OUT_DIR="$1_out"
 
 mkdir -p $OUT_DIR
@@ -19,5 +19,6 @@ for file in `ls $1`; do
     echo "Process $file"
     IN_FILE="$1/$file"
     OUT_FILE="$OUT_DIR/$file"
-    convert -fuzz 8% -trim +repage -resize $SIZE -gravity center -background white -extent $SIZE -enhance -colorspace OHTA -channel red -equalize -colorspace RGB $IN_FILE $OUT_FILE
+    convert -fuzz 5% -trim +repage -resize $SIZE -gravity center -background black -extent $SIZE $IN_FILE $OUT_FILE
+    # convert -fuzz 8% -trim +repage -resize $SIZE -gravity center -background black -extent $SIZE -enhance -colorspace OHTA -channel red -equalize -colorspace RGB $IN_FILE $OUT_FILE
 done
