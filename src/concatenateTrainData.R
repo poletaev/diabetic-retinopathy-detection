2ab36ef4d69f1c1b8558b696762b98132848ac29
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-07-27 16:49:53 +0600

    R script to cancatenate and split generated data.
    It might be launched as follows:
    
       Rscript src/concatenateTrainData.R

---
+++ src/concatenateTrainData.R
@@ -0,0 +1,30 @@
+
+library(caTools)
+
+train0 <- read.csv('data/train_0.txt', header=FALSE, stringsAsFactors=FALSE)
+train1 <- read.csv('data/train_1.txt', header=FALSE, stringsAsFactors=FALSE)
+train2 <- read.csv('data/train_2.txt', header=FALSE, stringsAsFactors=FALSE)
+train3 <- read.csv('data/train_3.txt', header=FALSE, stringsAsFactors=FALSE)
+train4 <- read.csv('data/train_4.txt', header=FALSE, stringsAsFactors=FALSE)
+
+train0$level <- rep(0, nrow(train0))
+train1$level <- rep(1, nrow(train1))
+train2$level <- rep(2, nrow(train2))
+train3$level <- rep(3, nrow(train3))
+train4$level <- rep(4, nrow(train4))
+
+train<-rbind(train0, train1, train2, train3, train4)
+train<-train[sample.int(nrow(train)),]
+rm(train0, train1, train2, train3, train4)
+
+# lets see how levels are spread after data preprocessing:
+hist(train$level)
+
+split <- sample.split(train$level, SplitRatio = 0.9)
+trainData <- train[split == TRUE,]
+crossValidationData <- train[split == FALSE,]
+
+# for Caffe we need file with image filenames
+# and label separated by space
+write.table(trainData, file="train.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
+write.table(crossValidationData, file="cv.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
