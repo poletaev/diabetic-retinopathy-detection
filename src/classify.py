f073270c1bb46fa6edcf1033b9c19d51745c2c33
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-07-28 03:19:35 +0000

    Modify classify.py script

--- src/classify.py
+++ src/classify.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python
-"""
-classify.py is an out-of-the-box image classifer callable from the command line.
-
-By default it configures and runs the Caffe reference ImageNet model.
-"""
 import numpy as np
 import caffe
 import os
 import pandas as pd
+import time
 
 def main():
-    caffe.set_mode_cpu()
-    net = caffe.Net('../data/deploy.prototxt',
-                    '../data/bvlc_googlenet_kaggle_iter_11000.caffemodel',
-                    caffe.TEST)
+    caffe.set_mode_gpu()
     test_data_folder = '../data/testdata'
     submission = pd.read_csv('../submissions/sampleSubmission.csv')
 
-    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-    transformer.set_transpose('data', (2, 0, 1))
-    transformer.set_raw_scale('data', 255)
-    transformer.set_channel_swap('data', (2, 1, 0))
+    print 'loading data'
+    paths = map(lambda x: os.path.join(test_data_folder, x + '.jpeg'), submission['image'])
+    inputs = [caffe.io.load_image(im_f) for im_f in paths]
 
-    net.blobs['data'].reshape(1, 3, 224, 224)
+    print 'loading model'
+    clf = caffe.Classifier('/mnt/caffe/models/bvlc_googlenet/deploy.prototxt',
+                           '/mnt/caffe/models/bvlc_googlenet/bvlc_googlenet_kaggle_iter_9000.caffemodel')
 
-    for f in os.listdir(test_data_folder):
-        net.blobs['data'].data[...] = transformer.preprocess('data',
-        caffe.io.load_image(os.path.join(test_data_folder, f)))
-        out = net.forward()
-        prediction = out['prob'].argmax()
-        submission.loc[submission.image == f.rstrip('.jpeg'), 'level'] = \
-            prediction
-        print("Predicted class for {} is {}.".format(f, prediction))
-    submission.to_csv('../data/submission.csv', index=False)
+    print 'classifying test set'
+    start = time.time()
+    predictions = clf.predict(inputs)
+    print 'done in %.2f s.' % (time.time() - start)
+
+    print 'saving result'
+    new_submission = pd.DataFrame(data={'image': submission['image'],
+                                        'level': predictions.argmax(axis=1)})
+    new_submission.to_csv('../submissions/fine-tuned-bvlc-googlenet-submission.csv', index=False)
 
 if __name__ == '__main__':
     main()
