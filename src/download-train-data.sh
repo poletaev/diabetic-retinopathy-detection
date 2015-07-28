5aadaca4a47d2123bb3fc520a1d8d4a9ddcfbc70
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-07-28 12:29:13 +0600

    Add files for download train and test data.

---
+++ src/download-train-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+declare -a arr=( "https://www.kaggle.com/c/diabetic-retinopathy-detection/download/train.zip.001"
+		 "https://www.kaggle.com/c/diabetic-retinopathy-detection/download/train.zip.002"
+		 "https://www.kaggle.com/c/diabetic-retinopathy-detection/download/train.zip.003"
+		 "https://www.kaggle.com/c/diabetic-retinopathy-detection/download/train.zip.004"
+		 "https://www.kaggle.com/c/diabetic-retinopathy-detection/download/train.zip.005")
+
+for i in "${arr[@]}"
+do
+   echo "$i"
+   curl "$i" -H 'Cookie: <place your cookie here>' -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36' -H 'Accept: image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Connection: keep-alive' -H 'If-Modified-Since: Fri, 17 Apr 2015 15:41:49 GMT' -H 'Referer: https://www.kaggle.com/c/diabetic-retinopathy-detection/data' --compressed -O -J -L
+done
