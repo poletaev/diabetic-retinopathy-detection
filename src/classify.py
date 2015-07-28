cd7c9a7e82fa41c79aa165872933b77aff86d4dd
Andrey Poletaev <poletaev.andrew@gmail.com>
2015-07-28 06:07:13 +0000

    Modify classify.py to process test images in batches.

--- src/classify.py
+++ src/classify.py
@@ -7,12 +7,12 @@ import time
 
 def main():
     caffe.set_mode_gpu()
+    caffe.set_device(1)
     test_data_folder = '../data/testdata'
     submission = pd.read_csv('../submissions/sampleSubmission.csv')
 
     print 'loading data'
     paths = map(lambda x: os.path.join(test_data_folder, x + '.jpeg'), submission['image'])
-    inputs = [caffe.io.load_image(im_f) for im_f in paths]
 
     print 'loading model'
     clf = caffe.Classifier('/mnt/caffe/models/bvlc_googlenet/deploy.prototxt',
@@ -20,8 +20,16 @@ def main():
 
     print 'classifying test set'
     start = time.time()
-    predictions = clf.predict(inputs)
-    print 'done in %.2f s.' % (time.time() - start)
+    predictions = []
+    batch_size = 64
+    for i in range(0, len(paths), batch_size):
+        batch_start = time.time()
+        print 'processing {} batch of {}'.format(i/batch_size + 1, len(paths)/batch_size + 1)
+        inputs = [caffe.io.load_image(im_f) for im_f in paths[i:i + batch_size]]
+        predictions.append(clf.predict(inputs))
+        print 'done in %.2f s.' % (time.time() - batch_start)
+    predictions = np.vstack(predictions)
+    print 'overall processing took %.2f s.' % (time.time() - start)
 
     print 'saving result'
     new_submission = pd.DataFrame(data={'image': submission['image'],
